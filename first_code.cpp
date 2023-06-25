e06968bdbbfb494ef243c1592bab817a0c34057b
Sumit Singh <sumit1315singh@gmail.com>
2023-06-25 21:21:38 +0530

    some modification done

--- first_code.cpp
+++ first_code.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 
 int main(){
-    std :: cout << "Hii... I am Sumit.";
+    std :: cout << "Hii... I am Sumit. Please enter three numbers to get the sum\n";
+    double a,b,c;
+    std::cin >> a >> b >> c;
+    std::cout << "the sum is " << a+b+c << std::endl;
     return 0;
 }
